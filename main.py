7a1a8d76c624f008892e8668976cc6e509d54c99
karolgawinek <163434770+karolgawinek@users.noreply.github.com>
2024-04-16 17:02:50 +0200

    wip: read code refactor to funcktion

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ users: list = [
     {"name": "Adrian", "surname": "Dobrzanski", "posts": 1, }
 
 ]
-print("Informacje o Twoich znajomych: ")
-for user in users:
-    print(f'\tTwoj znajomy {user["name"]} {user["surname"]} opublikował(a) {user["posts"]} postów')
+
+def read_friends(users: list)->None:
+    print("Informacje o Twoich znajomych: ")
+    for user in users:
+        print(f'\tTwoj znajomy {user["name"]} {user["surname"]} opublikował(a) {user["posts"]} postów')
+
+
+read_friends(users)
+
