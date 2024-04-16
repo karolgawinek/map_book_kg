104be7160ba824c8e902b2fed3c310ee6004c51c
karolgawinek <163434770+karolgawinek@users.noreply.github.com>
2024-04-16 18:51:33 +0200

    wip: add new menu function, add user search

--- models/data_source.py
+++ models/data_source.py
@@ -2,6 +2,6 @@ users: list = [
 
     {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
     {"name": "Hubert", "surname": "Sybilski", "posts": 1500000, },
-    {"name": "Adrian", "surname": "Dobrzanski", "posts": 1, }
-
+    {"name": "Adrian", "surname": "Dobrzanski", "posts": 1, },
+    {"name": "Bartek", "surname": "Wyrzykowski", "posts": 25, }
 ]
