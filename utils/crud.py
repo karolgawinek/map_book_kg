e2d4830a6ed08a0cbbac5e6d6b9e6a5728cce71c
karolgawinek <163434770+karolgawinek@users.noreply.github.com>
2024-04-16 18:14:16 +0200

    wip: add new menu function, add user append

--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,10 @@ def read_friends(users: list)->None:
     print("Informacje o Twoich znajomych: ")
     for user in users:
         print(f'\tTwoj znajomy {user["name"]} {user["surname"]} opublikował(a) {user["posts"]} postów')
+
+def add_user(liasta: list) -> None:
+    imie = input("Podaj imie: ")
+    nazwisko = input("Podaj nazwisko: ")
+    liczba_postow = int(input("Podaj liczbe postow uzytkownika: "))
+    new_user = {"name": imie, "surname": nazwisko, "posts": liczba_postow, }
+    lista.append(new_user)
